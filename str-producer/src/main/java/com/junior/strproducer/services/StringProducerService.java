f53f35a86a638165d4ba70e3a671af6b096a8ad2
Junior Barros <juniorbaarros@gmail.com>
2022-11-16 01:22:04 -0300

    feat: add function callback

--- str-producer/src/main/java/com/junior/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/junior/strproducer/services/StringProducerService.java
@@ -1,10 +1,12 @@
 package com.junior.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class StringProducerService {
@@ -12,6 +14,11 @@ public class StringProducerService {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(success -> {
+            if(success != null){
+                log.info("Send message with success {}", message);
+                log.info("Partition {}, Offset {}", success.getRecordMetadata().partition(), success.getRecordMetadata().offset());
+            }
+        }, error -> log.error("Error send message"));
     }
 }
