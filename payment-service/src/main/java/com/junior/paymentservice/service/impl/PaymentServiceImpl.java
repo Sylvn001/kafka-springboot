9b052433922642f1b9d758891fe50431eb86aea3
Junior Barros <juniorbaarros@gmail.com>
2022-12-26 18:31:39 -0300

    feat: created dockerfile to generate image and new configs in docker-compose to enable service

--- payment-service/src/main/java/com/junior/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/junior/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,27 @@ package com.junior.paymentservice.service.impl;
 
 import com.junior.paymentservice.model.Payment;
 import com.junior.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Service
 @Log4j2
 public class PaymentServiceImpl  implements PaymentService {
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+       Thread.sleep(1000);
+       log.info("Enviando pagamento ...");
+
+       kafkaTemplate.send("payment-topic", payment);
     }
 }
