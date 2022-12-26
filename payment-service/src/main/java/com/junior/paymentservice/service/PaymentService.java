f8964196d470d4e4df4d15ce6f68662991ed9c23
Junior Barros <juniorbaarros@gmail.com>
2022-12-26 16:56:47 -0300

    feat: created project to request payments

---
+++ payment-service/src/main/java/com/junior/paymentservice/service/PaymentService.java
@@ -0,0 +1,8 @@
+package com.junior.paymentservice.service;
+
+import com.junior.paymentservice.model.Payment;
+
+public interface PaymentService {
+    void sendPayment(Payment payment);
+
+}
---
+++ payment-service/src/test/java/com/junior/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
